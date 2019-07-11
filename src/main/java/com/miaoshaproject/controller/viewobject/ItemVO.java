a7ef64dd1c8e8e6621132e1d0275b40e8a4cde6b
zhangziqi1988 <zhangziqi1988@126.com>
2019-07-11 21:25:57 +0800

    添加商品模块

--- src/main/java/com/miaoshaproject/controller/viewobject/ItemVO.java
+++ src/main/java/com/miaoshaproject/controller/viewobject/ItemVO.java
@@ -1,36 +1,30 @@
-package com.miaoshaproject.service.model;
+package com.miaoshaproject.controller.viewobject;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-public class ItemModel {
+public class ItemVO {
 
-    private Integer id;
+   private Integer id;
 
     //商品名称
-    @NotBlank(message = "商品名称不能为空")
     private String title;
 
     //商品价格
-    @NotNull(message = "商品价格不能为空")
-    @Min(value = 0,message = "商品价格必须大于0")
     private BigDecimal price;
 
     //商品的库存
-    @NotNull(message = "库存不能不填")
     private Integer stock;
 
     //商品的描述
-    @NotBlank(message = "商品描述信息不能为空")
     private String description;
 
     //商品的销量
     private Integer sales;
 
     //商品描述图片的url
-    @NotBlank(message = "图片信息不能为空")
     private String imgUrl;
 
     public Integer getId() {
