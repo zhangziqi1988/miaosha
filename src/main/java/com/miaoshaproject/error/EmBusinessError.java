a30c4095abcc46effcd75a11280b6740e252acc5
zhangziqi1988 <zhangziqi1988@126.com>
2019-07-13 21:52:27 +0800

    添加下单模块

--- src/main/java/com/miaoshaproject/error/EmBusinessError.java
+++ src/main/java/com/miaoshaproject/error/EmBusinessError.java
@@ -6,7 +6,11 @@ public enum EmBusinessError implements CommonError {
     UNKOWN_ERROR(10002, "未知错误"),
 
     USER_NOT_EXIST(10001, "用户不存在"),
-    USER_LOGIN_FAIL(20002,"用户手机号或密码不正确")
+    USER_LOGIN_FAIL(20002,"用户手机号或密码不正确"),
+    USER_NOT_LOGIN(20003,"用户还未登陆"),
+
+    //30000开头为交易信息错误定义
+    STOCK_NOT_ENOUGH(30001,"库存不足"),
     ;
 
     private EmBusinessError(int errCode, String errMsg) {
