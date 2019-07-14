97f2cc20117ff6c7a6aa5b8048ca5f4f8932259e
zhangziqi1988 <zhangziqi1988@126.com>
2019-07-14 21:31:09 +0800

    完成秒杀模块，项目完成

--- src/main/java/com/miaoshaproject/controller/OrderController.java
+++ src/main/java/com/miaoshaproject/controller/OrderController.java
@@ -27,7 +27,8 @@ public class OrderController extends BaseController {
     @RequestMapping(value = "/createorder", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
     @ResponseBody
     public CommonReturnType createOrder(@RequestParam(name = "itemId") Integer itemId,
-                                        @RequestParam(name = "amount") Integer amount) throws BusinessException {
+                                        @RequestParam(name = "amount") Integer amount,
+                                        @RequestParam(name = "promoId",required = false) Integer promoId) throws BusinessException {
 
         Boolean isLogin=(Boolean)httpServletRequest.getSession().getAttribute("IS_LOGIN");
         if (isLogin == null || !isLogin.booleanValue()) {
@@ -38,7 +39,7 @@ public class OrderController extends BaseController {
         UserModel userModel = (UserModel) httpServletRequest.getSession().getAttribute("LOGIN_USER");
 
 
-        OrderModel orderModel=orderService.createOrder(userModel.getId(), itemId, amount);
+        OrderModel orderModel=orderService.createOrder(userModel.getId(), itemId, promoId,amount);
         return CommonReturnType.create(null);
     }
 
